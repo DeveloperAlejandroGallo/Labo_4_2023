bc3072d8070f217c6ca3f7e6663403e65ce6adf7
Alejandro Gallo <alegallo@gmail.com>
2023-03-30 22:21:38 -0300

    Primer y Segundo ejercicio

---
+++ PrimerosPasos/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment={
+    production:false,
+    endPoint: "http://localhost:5083/",
+  firebase: {
+    projectId: 'labo4-aleg',
+    appId: '1:621586958811:web:3521ba8a0b50521f34fffe',
+    databaseURL: 'https://labo4-aleg.firebaseio.com',
+    storageBucket: 'labo4-aleg.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyBCXgFPybYchlOgC_XMPUKoCiFcvYcZYZY',
+    authDomain: 'labo4-aleg.firebaseapp.com',
+    messagingSenderId: '621586958811',
+  }
+}
---
+++ comandos utiles.ps1
@@ -0,0 +1,10 @@
+
+npm --version
+
+npm install - @angular/cli
+
+ng serve #inicializa la app
+
+ng add @angular/fire 
+
+firebase init
